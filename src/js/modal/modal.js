ac914db07cf5c4eca8a740cc1c8c262a9e05a28d
EndyPlus <aapliusnin@gmail.com>
2025-11-06 22:14:57 +0200

    added: full adaptation; added: no scroll for body when modal is opened.

--- src/js/modal/modal.js
+++ src/js/modal/modal.js
@@ -1,12 +1,15 @@
+const body = document.querySelector("body");
 const modal = document.querySelector(".modal");
 const modalBackdrop = document.querySelector(".modal-backdrop");
 
 export function showModal() {
   modal.classList.remove("hidden");
+  body.classList.add("no-scroll");
 }
 
 export function hideModal() {
   modal.classList.add("hidden");
+  body.classList.remove("no-scroll");
 }
 
 modalBackdrop.addEventListener("click", hideModal);
